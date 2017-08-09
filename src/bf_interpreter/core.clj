1232578eec7ff392f5595582f4f198ca1dc49e77
mvukic <matija.vukic@hotmail.com>
2017-08-09 22:38:44 +0200

    implemented, not working

--- src/bf_interpreter/core.clj
+++ src/bf_interpreter/core.clj
@@ -2,16 +2,58 @@
 	(:gen-class))
 
 
-	(def program-pointer (ref 0)) 	;; points at current character in program
-	(def memory-pointer (ref 0))	;; points at urrent memory location
+	(def program-pointer (atom 0)) 	;; points at current character in program
+	(def memory-pointer (atom 0))	;; points at urrent memory location
 	(def memory (ref (byte-array 30)))	;; memory
 	(def program (vector))				;; program
-	(def brackets (ref {}))
+	(def brackets (atom {}))
+	(def running (atom true))			;; will indicate if parsing should stop
+	(def output (vector))				;; Vector of resulting bytes (chars)
 
-	(defn update-memory-pointer 
-		([] (dosync (alter memory-pointer + 1)))
-		([value] (dosync (ref-set memory-pointer value)))
-		)
+	(defn memory-pointer-inc
+		"Increase memory pointer by 1."
+		[]
+		(swap! memory-pointer inc)
+	)
+
+	(defn memory-pointer-dec
+		"Decrease memory pointer by 1."
+		[]
+		(swap! memory-pointer dec)
+	)
+
+	(defn program-pointer-inc
+		"Increase program pointer by 1."
+		[]
+		(swap! program-pointer inc)
+	)
+
+	(defn program-pointer-dec
+		"Decrease program pointer by 1."
+		[]
+		(swap! program-pointer dec)
+	)
+
+	(defn program-pointer-value
+		"Set program pointer to value."
+		[value]
+		(reset! program-pointer value)
+	)
+
+	(defn increase-memory
+		"Increases memory at location memory-pointer
+			memory[memory-pointer] += 1
+		"
+		[]
+		(def memory (update-in memory [@memory-pointer] inc))
+	)
+	(defn decrease-memory
+		"Decrease memory at location memory-pointer
+			memory[memory-pointer] -= 1
+		"
+		[]
+		(def memory (update-in memory [@memory-pointer] dec))
+	)
 
 	(defn read-file
 		"Opens file and reads chars to vector."
@@ -34,14 +76,26 @@
 	)
 
 	(defn find-brackets-pairs
-		"Finds pairs of brackets"
+		"Finds pairs of brackets."
 		[]
-		(let [
-			end-pose 0
-			counter 1
-		]
-			(for [item (map-indexed vector program)]
-				(println "w")
+		(doseq [[index item] (map-indexed vector program)]
+			(if (= item \[) 
+				(let [ end-pos (atom index) counter (atom 1)]
+				(while (> @counter 0)
+					(do
+						(swap! end-pos inc)
+						(if (= (get program @end-pos) \[)
+							(swap! counter inc))
+						(if (= (get program @end-pos) \])
+							(swap! counter dec))
+					)	
+				)
+				;; Save bracket pair
+				;; Index of opening bracket is key
+				;; Index of closing key is value
+				(swap! brackets assoc index @end-pos)
+				(swap! brackets assoc @end-pos index)
+				)
 			)
 		)
 	)
@@ -49,8 +103,51 @@
 	(defn real-parse
 		"Real parsing happens here."
 		[]
-		(println "real parsing")
+		(while (= running true)
+			(case (get program @program-pointer)
+				\> (do
+					(memory-pointer-inc)
+					(program-pointer-inc)
+				)
+				\< (do
+					(memory-pointer-dec)
+					(program-pointer-inc)
+				)
+				\+ (do
+					(increase-memory)
+					(program-pointer-inc)
+				)
+				\- (do
+					(decrease-memory)
+					(program-pointer-inc)
+				)
+				\. (do
+					(def output (conj output (get memory @memory-pointer)))
+					(program-pointer-inc)
+				)
+				\, (println "Implement reading from stdin")
+				\[ (do
+					(if (= (get memory @memory-pointer) 0)
+						(program-pointer-value (brackets @program-pointer)))
+					(program-pointer-inc)
+				)
+				\] (if (not= (get memory @memory-pointer) 0)
+					(program-pointer-value (brackets @program-pointer))
+					(program-pointer-inc)	
+				)
+			)
+			(if (>= @program-pointer (count program))
+				(swap! running false)
+			)
+			
+		)
 
+	)
+
+	(defn print-result 
+		"Prints resulting memory."
+		[]
+		(println "END")
 	)
 	
 	(defn parse
@@ -74,6 +171,7 @@
 		(do 
 			(read-file (first args)) 
 			(parse)
+			(print-result)
 		) 
 		(exit))
 	)
